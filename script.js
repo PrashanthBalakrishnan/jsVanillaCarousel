cd06c126f366531a79400799005a9d3542047148
unknown <prashanthdilshaan@gmail.com>
2022-06-19 23:49:35 -0400

    added function to next button
    made next button change image using for of loop

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
 let nextBtn = document.getElementById("carousel-btn-next");
 let prevBtn = document.getElementById("carousel-btn-prev");
-let carouselItem = document.getElementsByClassName("carousel-item");
-
+const slides = document.getElementsByClassName("carousel-item");
 let slidePosition = 0;
-
-for (let i = 0; i < carouselItem.length; i++) {
-    const slide = i;
-    const totalSlide = carouselItem.length
+const totalSlide = slides.length;
 
 
+function hideAllSlides() {
+    for (let slide of slides) {
+        slide.classList.remove("carousel-item-visible")
+        slide.classList.add("carousel-item-hidden")
+        console.log(slide)
+    }
 }
 
-nextBtn.addEventListener("click", function () {
-    slidePosition++;
-    console.log(slidePosition);
-})
+nextBtn.addEventListener("click", moveToNextSlide);
+
+prevBtn.addEventListener("click", moveToPrevSlide);
+
+function moveToNextSlide() {
+    console.log("next button clicked")
+    hideAllSlides()
+
+    if (slidePosition === totalSlide - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
+function moveToPrevSlide() {
+    console.log("prev button clicked")
+    hideAllSlides()
+
+    if (slidePosition === 0) {
+        slidePosition = totalSlide - 1;
+    } else {
+        slidePosition--;
+    }
+
+    slides[slidePosition].classList.add("carousel-item-visible");
+}
+
+
+
